67f47f8269c78e365bcc06cc5034e4218a86b6f4
Guillermo <guillermo.triguero@nestle.com>
2025-07-05 20:34:59 +0200

    check locators register form

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -14,3 +14,19 @@ test('create user', async ({ page }) => {
   console.log('Username: ' + user.username);
   console.log('Password: ' + user.password);
 });
+
+test('create user with custom data', async ({ page }) => {
+  await page.goto('https://parabank.parasoft.com/parabank/register.htm', { timeout: 60000 });
+  await page.locator('[id="customer.firstName"]').fill('John');
+  await page.locator('[id="customer.lastName"]').fill('Doe');
+  await page.locator('[id="customer.address.street"]').fill('123 Main St');
+  await page.locator('[id="customer.address.city"]').fill('Anytown');
+  await page.locator('[id="customer.address.state"]').fill('CA');
+  await page.locator('[id="customer.address.zipCode"]').fill('12345');
+  await page.locator('[id="customer.phoneNumber"]').fill('123-456-7890');
+  await page.locator('[id="customer.ssn"]').fill('123-45-6789');
+  await page.locator('[id="customer.username"]').fill('johndoe');
+  await page.locator('[id="customer.password"]').fill('password123');
+  await page.locator('[id="repeatedPassword"]').fill('password123');
+ await page.locator('[value="Register"]').click();
+});
