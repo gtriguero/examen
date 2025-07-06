a279cf24975faedf040b9307159089ef23e3d9c1
Guillermo <guillermo.triguero@nestle.com>
2025-07-06 20:11:24 +0200

    workflow completo separado

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,17 +1,9 @@
 import { expect } from '@playwright/test';
 import { test } from './fixtures/fixture';
 
-import { createUser } from './models/users';
-
 test.beforeEach(async ({ pageManager }) => {
     await pageManager.onNavigation().goto();
 });
-
-test('create user', async ({ pageManager }) => {
-  const user = createUser();
-});
-
-
 
 test('register user', async ({ pageManager, user }) => {
 test.slow();
@@ -19,11 +11,8 @@ test.slow();
   await pageManager.onGeneralForm().clickRegisterButton();
   await pageManager.onRegisterComponent().fillRegisterForm(user);
   await pageManager.onGeneralForm().clickSubmitButton();
+  await expect(pageManager.onRegisterComponent().titleRightPanel).toHaveText('Welcome ' + user.username);
   await pageManager.onGeneralForm().clickLogoutButton();
-  await pageManager.onGeneralForm().clickLoginButton();
+  await expect(pageManager.onGeneralForm().registerButton).toBeVisible();
   await pageManager.onLoginComponent().fillLoginForm(user);
-/*
-test.only('Register form', async ({ pageManager, user }) => {
-    await pageManager.onRegisterComponent().fillForm(user);
-    await expect(pageManager.onHomeComponent().lightOnDiv).toBeVisible()^*/
 });
